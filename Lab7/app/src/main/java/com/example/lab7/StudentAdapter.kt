06f768f93997f18badbf8d36426a36f7b97e9276
siriluk5322 <69790187+siriluk5322@users.noreply.github.com>
2020-09-14 17:04:02 +0700

    Lab7 Sec3 613020532-2

---
+++ Lab7/app/src/main/java/com/example/lab7/StudentAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.lab7
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.std_item_layout.view.*
+
+class StudentsAdapter (val item: List<Student>, val context:Context): RecyclerView.Adapter<ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view_item = LayoutInflater.from(parent.context).inflate(R.layout.std_item_layout,parent,false)
+        return ViewHolder(view_item)
+    }
+    override fun getItemCount(): Int {
+        return item.size
+    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.tvID.text = "ID : "+ item[position].id
+        holder.tvName.text = "Name : "+ item[position].name
+        holder.tvAge.text = "Age : "+ item[position].age.toString()
+    }
+}
+
